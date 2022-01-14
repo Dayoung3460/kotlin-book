ec3f1568e591a58a0b60dfb09aedb735f89109cb
클라라 (김다영) <clara@coinvest.kr>
2022-01-14 22:27:54 +0900

    array copy

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -1,28 +1,13 @@
 fun main(args: Array<String>) {
-    val countries = arrayOf("korea", "usa", "japan", "china", "russia")
-    val cities = arrayOf("seoul", "w.s.", "tokyo", "b.j.", "m.b.")
-    for(i in countries) {
-        print("$i ")
+    var original = arrayOf(1, 2, 3, 4, 5)
+    var copy = Array(original.size){0}
+
+    for((idx, i) in original.withIndex()) {
+        copy[idx] = i
     }
-    println()
-    for(i in cities) {
-        print("$i ")
-    }
-    println()
 
-    while(true) {
-        val selectedIdx = kotlin.random.Random.nextInt(5)
-
-        print("${countries[selectedIdx]}의 수도는? ")
-        var capital = readLine()!!.toInt()
-
-        when(capital) {
-            selectedIdx -> {
-                println("정답")
-                break
-            }
-            else -> println("오답")
-        }
+    for(v in copy) {
+        println("$v ")
     }
 
 
