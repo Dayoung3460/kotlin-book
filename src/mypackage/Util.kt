6ea1bef32b14ce56fa7bead39145d14eed980fde
클라라 (김다영) <clara@coinvest.kr>
2022-02-07 11:06:52 +0900

    function reference2

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -1,18 +1,9 @@
 package mypackage
 
-fun multiplyTwo(n: Int) = n * 2
-fun multiplyThree(n: Int) = n * 3
-fun isEven(n: Int) = (n % 2) === 0
+fun printReverse(s: String) = println(s.reversed())
 
 
 fun main(args: Array<String>) {
-    var myMultipleFuncRef: (Int) -> Int = ::multiplyTwo
-    println(myMultipleFuncRef(2))
-
-    myMultipleFuncRef = ::multiplyThree
-    println(myMultipleFuncRef(2))
-
-    var isEvenRef = ::isEven
-    println(isEvenRef(3))
+    listOf("Hello", "World").forEach(::printReverse)
 }
 
