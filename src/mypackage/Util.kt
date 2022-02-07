b09b58ccfb88b786a13e35b99d18612a07f6a46d
클라라 (김다영) <clara@coinvest.kr>
2022-02-07 10:55:58 +0900

    function overloading2

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -1,14 +1,19 @@
 package mypackage
 
-fun square(x: Int) = x * x
-fun square(x: Long) = x * x
-fun square(x: Float) = x * x
-fun square(x: Double) = x * x
+fun overloadingTest() = println("overloading test 1")
+fun overloadingTest(x: Int) = println("overloading test 2")
+fun overloadingTest(x: Int, y: Int) = println("overloading test 3")
+fun overloadingTest(x: Int, y: Double) = println("overloading test 4")
+fun overloadingTest(x: Double, y: Int) = println("overloading test 5")
+fun overloadingTest(x: Int, y: Int, z: Int): Int = x + y + z
+
 
 fun main(args: Array<String>) {
-    println(square(2))
-    println(square(3L))
-    println(square(4.0F))
-    println(square(5.0))
+    overloadingTest()
+    overloadingTest(1)
+    overloadingTest(2, 3)
+    overloadingTest(2.0, 3)
+    println(overloadingTest(1, 2, 3))
+
 }
 
