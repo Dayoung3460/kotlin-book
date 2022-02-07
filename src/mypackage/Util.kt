b588c8658819715f429f3921affda35530f48753
클라라 (김다영) <clara@coinvest.kr>
2022-02-07 14:54:56 +0900

    function reference3

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -1,9 +1,30 @@
 package mypackage
 
-fun printReverse(s: String) = println(s.reversed())
+class StringAppender(init: String) {
+    var result = init
+    fun append(s: String) {
+        result += s
+    }
+    class Inner {
+        companion object {
+            fun myPrintln(any: Any) = println("${any.toString()}")
+        }
+    }
+    companion object {
+        fun myPrintln(any: Any) = println("${any.toString()}")
+    }
+}
 
 
 fun main(args: Array<String>) {
-    listOf("Hello", "World").forEach(::printReverse)
+    val appender = StringAppender("")
+    listOf("Hello", "World").forEach(appender::append)
+    println(appender.result)
+
+    var myPrintlnRef = (StringAppender)::myPrintln
+    myPrintlnRef("Hello World")
+    myPrintlnRef = (StringAppender.Inner)::myPrintln
+    myPrintlnRef("Hello World")
+
 }
 
