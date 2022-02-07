87475a6abf43eec4f11b402a40d31cb155fa2096
클라라 (김다영) <clara@coinvest.kr>
2022-02-07 11:03:15 +0900

    function reference

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -1,19 +1,18 @@
 package mypackage
 
-fun overloadingTest() = println("overloading test 1")
-fun overloadingTest(x: Int) = println("overloading test 2")
-fun overloadingTest(x: Int, y: Int) = println("overloading test 3")
-fun overloadingTest(x: Int, y: Double) = println("overloading test 4")
-fun overloadingTest(x: Double, y: Int) = println("overloading test 5")
-fun overloadingTest(x: Int, y: Int, z: Int): Int = x + y + z
+fun multiplyTwo(n: Int) = n * 2
+fun multiplyThree(n: Int) = n * 3
+fun isEven(n: Int) = (n % 2) === 0
 
 
 fun main(args: Array<String>) {
-    overloadingTest()
-    overloadingTest(1)
-    overloadingTest(2, 3)
-    overloadingTest(2.0, 3)
-    println(overloadingTest(1, 2, 3))
+    var myMultipleFuncRef: (Int) -> Int = ::multiplyTwo
+    println(myMultipleFuncRef(2))
 
+    myMultipleFuncRef = ::multiplyThree
+    println(myMultipleFuncRef(2))
+
+    var isEvenRef = ::isEven
+    println(isEvenRef(3))
 }
 
