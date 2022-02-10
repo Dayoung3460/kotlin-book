7fc1ee1e7cea0153dd5f8ec5690a1f839c079482
클라라 (김다영) <clara@coinvest.kr>
2022-02-10 19:55:26 +0900

    with, run, apply, also

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -5,10 +5,35 @@ data class Point(var x: Int = 0, var y: Int = 0)
 
 fun main(args: Array<String>) {
     var p1 = Point(1, 2)
-    p1?.let {
-        it.x *= 2
-        it.y *= 2
+
+    var printToPair = p1.run {
+        Pair(x, y)
     }
-    println(p1)
+//    println(printToPair)
+
+    var p3 = run {
+        var x = 100
+        var y = 200
+        Point(x, y)
+    }
+//    println(p3)
+
+    var p2 = p1.apply {
+        x = 100
+        y = 200
+        "Hello"
+    }
+//    println(p2)
+//    println(p1 === p2)
+
+    var words = mutableListOf("Hello", "World")
+    words.also {
+        println("first item: ${it.first()}")
+        println("last item: ${it.last()}")
+        println("list item: ${it.size}")
+    }.add("Kotlin")
+    println(words)
 }
+
+
 
