b629188e3cceacfd8e65f75726e54d82aa3a0c47
클라라 (김다영) <clara@coinvest.kr>
2022-02-10 20:40:49 +0900

    excludeChar

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -1,38 +1,27 @@
 package mypackage
 
-data class Point(var x: Int = 0, var y: Int = 0)
+import kotlin.random.Random
+
+fun excludeChar(word: String, array: Array<Char>): String {
+    var result = ""
+
+    for(w in word) {
+        if(w in array) {
+            continue
+        }
+        result += w
+    }
+    return result
+}
+
+
 
 
 fun main(args: Array<String>) {
-    var p1 = Point(1, 2)
-
-    var printToPair = p1.run {
-        Pair(x, y)
-    }
-//    println(printToPair)
-
-    var p3 = run {
-        var x = 100
-        var y = 200
-        Point(x, y)
-    }
-//    println(p3)
-
-    var p2 = p1.apply {
-        x = 100
-        y = 200
-        "Hello"
-    }
-//    println(p2)
-//    println(p1 === p2)
-
-    var words = mutableListOf("Hello", "World")
-    words.also {
-        println("first item: ${it.first()}")
-        println("last item: ${it.last()}")
-        println("list item: ${it.size}")
-    }.add("Kotlin")
-    println(words)
+    var str1 = excludeChar("Hello", arrayOf('H', 'l'))
+    println(str1)
+    var str2 = excludeChar("Hello World", arrayOf('H', 'W', 'o'))
+    println(str2)
 }
 
 
