e358a948ee4cfd7cf5913e2d89d790fca6d7fc04
클라라 (김다영) <clara@coinvest.kr>
2022-02-10 19:42:26 +0900

    let

--- src/mypackage/Util.kt
+++ src/mypackage/Util.kt
@@ -4,15 +4,11 @@ data class Point(var x: Int = 0, var y: Int = 0)
 
 
 fun main(args: Array<String>) {
-    var nullableString: String? = null
-
-    nullableString?.let {
-        println(it.length)
+    var p1 = Point(1, 2)
+    p1?.let {
+        it.x *= 2
+        it.y *= 2
     }
-
-    var upper = nullableString?.let {
-        it.toUpperCase()
-    }
-    println(upper)
+    println(p1)
 }
 
