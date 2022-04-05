babd41817206a5f25d691c18645b52c704bdf7a9
김다영 (클라라) <kimdayoung771@gmail.com>
2022-04-05 17:57:54 +0900

    Companion Object2

--- src/classStudy_part2/CompanionObjectStudy.kt
+++ src/classStudy_part2/CompanionObjectStudy.kt
@@ -17,6 +17,21 @@ class CompanionObjectClass(var x: Int) {
   }
 }
 
+class Product(val name: String, val price: Double, var discountRate: Double) {
+  companion object {
+    fun calculatePrice(product: Product): Double {
+      return product.price - (product.price * product.discountRate)
+    }
+
+    fun calculateTotal(products: List<Product>): Double {
+      var total = 0.0
+      for(p in products)
+        total += Product.calculatePrice(p)
+      return total
+    }
+  }
+}
+
 fun main() {
   var c = CompanionObjectClass(100)
   c.instanceFunction()
@@ -25,4 +40,10 @@ fun main() {
   CompanionObjectClass.classFunc()
   println(CompanionObjectClass.classVariable)
   println(CompanionObjectClass.CLASS_CONSTANT)
+
+  val p1 = Product("toy", 1000.0, 0.1)
+  val p2 = Product("book", 5000.0, 0.0)
+  println(Product.calculatePrice(p1))
+  println(Product.calculateTotal(listOf(p1, p2)))
+
 }
