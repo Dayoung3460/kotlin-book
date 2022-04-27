7f9a5828e372e907f751f3c208776582e99050fc
김다영 (클라라) <kimdayoung771@gmail.com>
2022-04-27 15:07:57 +0900

    Extension Function2

--- src/challenges/ch07/ExtensionFunctionExercise.kt
+++ src/challenges/ch07/ExtensionFunctionExercise.kt
@@ -10,6 +10,20 @@ fun String.countSpace(): Int {
   return count
 }
 
+fun Double.toPair(): Pair<Int, Double> {
+  var intNum = this.toInt()
+  var decNum = this - intNum
+  return intNum to decNum
+}
+
 fun main() {
   println("Hello world".countSpace())
+
+  println("-----------------------")
+
+  var d1 = 1.5
+  var d2 = 3.14159
+  println(d1.toPair())
+  println(d2.toPair())
+
 }
