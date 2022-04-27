20e9ea4bc882d9a9ee1c0701aceeae06929989b9
김다영 (클라라) <kimdayoung771@gmail.com>
2022-04-27 13:25:28 +0900

    Calculable Interface

--- src/challenges/ch06/CalculableInterfaceExercise.kt
+++ src/challenges/ch06/CalculableInterfaceExercise.kt
@@ -4,6 +4,10 @@ interface Calculate {
   fun calculate(a: Double, b: Double): Double
 }
 
+interface Calculable {
+  fun calculable(x: Double, y: Double): Double
+}
+
 class Add: Calculate {
   override fun calculate(a: Double, b: Double) = a + b
 }
@@ -12,7 +16,16 @@ class Subtract: Calculate {
   override fun calculate(a: Double, b: Double) = a - b
 }
 
+class Multiply: Calculable {
+  override fun calculable(a: Double, b: Double) = a * b
+}
+
+class Divide: Calculable {
+  override fun calculable(a: Double, b: Double) = a / b
+}
+
 fun doCalculate(c: Calculate, a: Double, b: Double) = c.calculate(a, b)
+fun doCalculation(c: Calculable, a: Double, b: Double) = c.calculable(a, b)
 
 fun main() {
   var add = Add()
@@ -23,6 +36,16 @@ fun main() {
 
   println(res1)
   println(res2)
+
+  println("----------------------------------")
+
+  var mul = Multiply()
+  var div = Divide()
+
+  var res3 = doCalculation(mul, 3.0, 4.0)
+  var res4 = doCalculation(div, 10.0, 4.0)
+  println(res3)
+  println(res4)
 }
 
 
