360dc47662384c6754dc4c6c683a91516ceae303
김다영 (클라라) <kimdayoung771@gmail.com>
2022-04-27 13:47:09 +0900

    Data class2

--- src/challenges/ch07/DataClasses.kt
+++ src/challenges/ch07/DataClasses.kt
@@ -1,8 +1,26 @@
 package challenges.ch07
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 data class Movie(var title: String, var genre: String, var rate: Double)
+
+data class TwitterMessage(var nickname: String, val date: String, var message: String)
 
 fun main() {
   var movie = Movie("Frozen", "Animation", 9.0)
   println(movie)
+
+  println("---------------------------------")
+
+  var nowWithTime = LocalDate.now()
+  val month = nowWithTime.month.toString().lowercase().capitalize()
+  val dateFinal = "${month} ${nowWithTime.dayOfMonth}"
+//  var nowWithTime = LocalDateTime.now()
+//  var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+//  var formatted: String = nowWithTime.format(formatter)
+
+  var twitterMessage = TwitterMessage("programming", dateFinal, "blah blah blah")
+  println(twitterMessage)
 }
