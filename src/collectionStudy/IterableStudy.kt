3f6b8eab6e2b163005d2ceac81d9300c0eab2e68
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-24 15:34:02 +0900

    Iterable list class

--- src/collectionStudy/IterableStudy.kt
+++ src/collectionStudy/IterableStudy.kt
@@ -12,7 +12,38 @@ class FruitsIterable: Iterable<String> {
   }
 }
 
+class MyList<T>(vararg val args: T): Iterable<T> {
+  private class MyListIterator<T2>(val args: Array<T2>): Iterator<T2> {
+    var currentIdx = 0
+    override fun hasNext(): Boolean {
+      return currentIdx < args.size
+    }
+
+    override fun next(): T2 {
+      val r = args[currentIdx]
+      currentIdx++
+      return r
+    }
+  }
+  override fun iterator(): Iterator<T> {
+    return MyListIterator(args)
+  }
+}
+
 fun main() {
+  var myList1 = MyList(1, 2, 3, 4, 5)
+  for(item in myList1) {
+    print("$item ")
+  }
+  println()
+
+  var myList2 = MyList("Hello", "World", "Kotlin")
+  for(item in myList2) {
+    print("$item ")
+  }
+
+  println("---------------------------------------------------")
+
   val fruitsIterable = FruitsIterable()
   var iterator = fruitsIterable.iterator()
   while (true) {
