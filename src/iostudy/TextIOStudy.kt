294712b5b3e78c7c5a9cc3df26f82e891629a63f
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-24 17:13:23 +0900

    create file

---
+++ src/iostudy/TextIOStudy.kt
@@ -0,0 +1,27 @@
+package iostudy
+
+import java.io.File
+import java.util.*
+
+fun main() {
+  var f1 = File("file1.txt")
+   if(f1.createNewFile()) {
+     f1.writeText("Hello")
+   } else {
+     f1.writeText("World")
+   }
+
+  println(f1.length())
+  println(f1.extension)
+  println(f1.path)
+  println(f1.absolutePath)
+  println(f1.isFile)
+  println(f1.isDirectory)
+
+  var lastModified: Long = f1.lastModified()
+  println(lastModified)
+  println(Date(lastModified))
+  println(f1.exists())
+  f1.delete()
+}
+
