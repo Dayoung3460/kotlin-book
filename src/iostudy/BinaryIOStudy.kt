7ffff88090d836fd415d0ea1881b8a75590c0749
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-24 17:49:31 +0900

    use method

--- src/iostudy/BinaryIOStudy.kt
+++ src/iostudy/BinaryIOStudy.kt
@@ -45,5 +45,31 @@ fun main() {
   println(doubleFromFile)
   println(booleanFromFile)
   println(stringFromFile)
+
+  ObjectOutputStream(FileOutputStream(personFile)).use {
+    it.writeObject(p1)
+    it.writeObject(p2)
+    it.writeInt(100)
+    it.writeDouble(1.234)
+    it.writeBoolean(true)
+    it.writeUTF("Hello")
+  }
+
+  ObjectInputStream(FileInputStream(personFile)).use {
+    val p1FromFile = it.readObject() as Person
+    val p2FromFile = it.readObject() as Person
+
+    val intFromFile = it.readInt()
+    val doubleFromFile = it.readDouble()
+    val booleanFromFile = it.readBoolean()
+    val stringFromFile = it.readUTF()
+  }
+
+  println(p1FromFile)
+  println(p2FromFile)
+  println(intFromFile)
+  println(doubleFromFile)
+  println(booleanFromFile)
+  println(stringFromFile)
 }
 
