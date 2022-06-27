0db1378f11304e1f0491a11dc2d4cd3734a17181
김다영 (클라라) <kimdayoung771@gmail.com>
2022-06-27 13:11:26 +0900

    ch11 diary with Object

--- src/challenges/ch11/Diary.kt
+++ src/challenges/ch11/Diary.kt
@@ -1,7 +1,11 @@
 package challenges.ch11
 
 import java.io.File
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
 import java.time.LocalDateTime
+
+data class Diary (var content: String): java.io.Serializable
 
 fun main() {
   val today = LocalDateTime.now()
@@ -9,20 +13,36 @@ fun main() {
   val month = today.month.value
   val day = today.dayOfMonth
 
+  var content = ""
   var diaryFile = File("diary_$year-$month-$day.txt")
   diaryFile.createNewFile()
   while (true) {
     print("> ")
     val line = readLine()
 
-    if(line == "/quit") break
+    if(line == "/quit") {
+      val diary = Diary(content)
+      val oos = ObjectOutputStream(FileOutputStream(diaryFile))
+      oos.writeObject(diary)
+      oos.flush()
+      oos.close()
+      break
+    }
     if(line == "/clear") {
-      diaryFile.writeText("")
+      content = ""
     } else {
-//      !! will throw NullPointerException if the value is null
-      diaryFile.appendText("${line!!}\n")
+      content += "$line\n"
       println("\"$line\" entered.")
     }
+
+//    if(line == "/quit") break
+//    if(line == "/clear") {
+//      diaryFile.writeText("")
+//    } else {
+////      !! will throw NullPointerException if the value is null
+//      diaryFile.appendText("${line!!}\n")
+//      println("\"$line\" entered.")
+//    }
   }
 
 }
